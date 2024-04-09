c6fc241c665242673d0fa0ca420e1a34415fed3c
GIBSON J <gibson.25cs@licet.ac.in>
2024-04-09 17:48:51 +0530

    da!@#

---
+++ src/main/java/com/gibson/gibson/Controller/LinkedlnJobScraperController.java
@@ -0,0 +1,31 @@
+package com.gibson.gibson.Controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gibson.gibson.domain.LinkelnJobListing;
+import com.gibson.gibson.service.LinkedlnJobScraperService;
+
+@RestController
+@RequestMapping("/api/ljobs")
+public class LinkedlnJobScraperController {
+
+    @Autowired
+    private LinkedlnJobScraperService jobScraperService;
+
+    @GetMapping("/search")
+    public List<LinkelnJobListing> scrapeJobs(@RequestParam String keywords, @RequestParam String location) throws IOException {
+        String url = buildUrl(keywords, location);
+        return jobScraperService.scrapeJobs(url);
+    }
+
+    private String buildUrl(String keywords, String location) {
+        return "https://www.linkedin.com/jobs/search/?keywords=" + keywords + "&location=" + location;
+    }
+}
